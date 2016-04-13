d81b8b5f800f944ede3e0f49a8e59105731f2633
Evan Harvey <e1.0harvey@gmail.com>
2016-04-13 15:20:54 -0600

    storage: Updated RemoteDbOperations unit tests. 	 * Added unit tests for RemoteDbOperations 	 * Added unit tests for addUser.

--- app/src/test/java/storage/shapes/RemoteDatabaseTest.java
+++ app/src/test/java/storage/shapes/RemoteDatabaseTest.java
@@ -9,8 +9,24 @@ import static org.junit.Assert.*;
 public class RemoteDatabaseTest {
     @Test
     public void RemoteDatabaseOperationsTest() {
+        long seed;
+        boolean ret;
+
         RemoteDatabaseOperations remoteDatabaseOperations = new RemoteDatabaseOperations();
         assertNotEquals(null, remoteDatabaseOperations);
+
+        //remoteDatabaseOperations.getLoginStatus("testuser");
+
+        remoteDatabaseOperations.setBlockSeed("testuser", 1111);
+
+        seed = remoteDatabaseOperations.getBlockSeed("testuser");
+        assertEquals(1111, seed);
+
+        ret = remoteDatabaseOperations.addUser("testuser2", "password");
+        assertEquals(true, ret);
+
+        ret = remoteDatabaseOperations.addUser("testuser2", "password");
+        assertEquals(false, ret);
     }
 
 }
