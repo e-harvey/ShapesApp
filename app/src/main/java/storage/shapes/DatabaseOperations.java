fa736a9d8459116479cb8e3d7df1b1a902f7e006
Evan Harvey <e1.0harvey@gmail.com>
2016-04-15 02:29:56 -0600

    storage: Changed DatabaseOperations to be abstract. 	 * All methods within DatabaseOperations are not abstract. 	 * Updated methods to provide desired functionality for the 	 shapesApp -- the local and remote databases will stay 	 consistent with one another except for adding users. If 	 the remote database cannot be reached the user is only added 	 to the local database. We will need to prompt the user once 	 their network conneciton is restored.

--- app/src/main/java/storage/shapes/DatabaseOperations.java
+++ app/src/main/java/storage/shapes/DatabaseOperations.java
@@ -7,76 +7,122 @@ import java.util.ArrayList;
 /**
  * Created by Fritz on 4/7/2016.
  */
-public class DatabaseOperations  {
+public abstract class DatabaseOperations  {
+    static Context context;
+    static LocalDatabaseOperations local;
+    static RemoteDatabaseOperations remote;
 
-
-    Context context;
-    LocalDatabaseOperations local;
-    RemoteDatabaseOperations remote;
-
-    public DatabaseOperations(Context context) {
-        this.context = context;
+    public static void DatabaseOperationsInit(Context context) {
+        DatabaseOperations.context = context;
         LocalDatabaseOperations local = new LocalDatabaseOperations(context);
         RemoteDatabaseOperations remote = new RemoteDatabaseOperations();
     }
 
     // logs user out of databases
-    public void logout(String username) {
+    public static void logout(String username) {
         local.logout(username);
         remote.logout(username);
     }
 
     // logs user into databases. returns false if password/username do not match
-    public boolean login(String username, String password) {
-        return local.login(username, password);
+    public static boolean login(String username, String password)
+    {
+
+        if (!local.login(username, password)) {
+             return false;
+        }
+
+        if (!remote.login(username, password)) {
+            System.out.println("Play with friends not available.  Please check your network connection and credentials.");
+        }
+
+        return true;
     }
 
     // returns username of local user logged in from previous session. returns null if no users logged in.
-    public String getLocalLoggedInUser() {
+    public static String getLocalLoggedInUser()
+    {
         return local.getLocalLoggedInUser();
     }
 
     // set high score for user
-    public void setHighScore(String username, int score) {
+    public static void setHighScore(String username, int score)
+    {
         local.setHighScore(username, score);
         remote.setHighScore(username, score);
     }
 
     // return high score for user. returns integer high score
-    public long getHighScore(String username){
-        return local.getHighScore(username);
+    public static long getHighScore(String username)
+    {
+        if (remote.getLoginStatus(username))
+            return remote.getHighScore(username);
+        else
+            return local.getHighScore(username);
 
     }
 
     // add username to databases. returns false if username is already taken
-    public boolean addUser(String username, String password) {
-        boolean ret = local.addUser(username, password) | remote.addUser(username, password);
+    public static boolean addUser(String username, String password) {
+        boolean ret;
+        // Ensure username only contains valid characters
+        if (username.contains("\\"))
+            System.out.println("usernames may not contain \"\\\"");
+        if (username.contains("'"))
+            System.out.println("usernames may not contain \"'\"");
+
+        ret = local.addUser(username, password);
+
+        if (!remote.addUser(username, password)) {
+            System.out.println("user '"+ username +"' could not be added to the remote database");
+        }
+
         return ret;
     }
 
     // resets password for user. returns false if password does not match
-    public boolean setPassword(String username, String oldPassword, String newPassword) {
-        return local.setPassword(username, oldPassword, newPassword);
+    public static boolean setPassword(String username, String oldPassword, String newPassword)
+    {
+        boolean ret = true;
+
+        if (local.setPassword(username, oldPassword, newPassword))
+            ret = remote.setPassword(username, oldPassword, newPassword);
+        else
+            return false;
+
+        if (!ret)
+            local.setPassword(username, newPassword, oldPassword);
+
+        return false;
     }
 
     // deletes user from system. returns false if password does not match
-    public boolean deleteUser(String username, String password) {
-        remote.deleteUser(username, password);
-        return local.deleteUser(username, password);
+    public static boolean deleteUser(String username, String password) {
+        boolean ret = true;
+
+        if (local.deleteUser(username, password)) {
+            if (!remote.deleteUser(username, password)) {
+                local.addUser(username, password);
+                return false;
+            }
+        } else {
+            return false;
+        }
+        return true;
     }
 
     // returns an ArrayList including all friends for a particular user
-    public ArrayList<String> getFriendsList(String username) {
+    public static ArrayList<String> getFriendsList(String username) {
         return local.getFriendsList(username);
     }
 
     // adds a new friend to a user's friends list
-    public void addNewFriend(String usernameOwner, String usernameFriend) {
+    public static void addNewFriend(String usernameOwner, String usernameFriend) {
         local.addNewFriend(usernameOwner, usernameFriend);
     }
 
     // deletes a friend from a user's friends list
-    public void deleteFriend(String usernameOwner, String usernameFriend) {
+    public static void deleteFriend(String usernameOwner, String usernameFriend) {
         local.deleteFriend(usernameOwner, usernameFriend);
     }
 }
