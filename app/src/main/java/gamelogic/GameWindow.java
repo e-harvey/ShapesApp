e0cab7ac9535b44c1102bd052615abcd6e2fbf63
benjlouie <benjlouie@gmail.com>
2016-04-14 23:54:12 -0600

    fixed git messup

--- app/src/main/java/gamelogic/GameWindow.java
+++ app/src/main/java/gamelogic/GameWindow.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import java.util.ArrayList;
 
 import fthomas.shapes.R;
 
@@ -17,15 +20,36 @@ public class GameWindow extends SurfaceView implements SurfaceHolder.Callback
 {
     private GameThread gameThread;
     private Block[][] grid;
-    private final int XBlocks = 8; //blocks on x axis
-    private final int YBlocks = 10; //blocks on y axis
+    private final int XBlocks = 6; //blocks on x axis
+    private final int YBlocks = 8; //blocks on y axis
+    private int gridWidth;
+    private int gridHeight;
+    private int blockWidth;
+    private ArrayList<Bitmap> blockImages = new ArrayList<Bitmap>();
 
     public GameWindow(Context context)
     {
         super(context);
         grid = new Block[XBlocks][YBlocks];
-        //TODO: fill array with new Blocks here
-        //grid[0][0] = new Block(Block.BlockType.WEDGE, 20, 20);
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        gridWidth = metrics.widthPixels;
+        gridHeight = (int)(metrics.widthPixels * ((float)YBlocks / XBlocks));
+        blockWidth = gridWidth / XBlocks;
+
+        // 0 empty, 1 wedge, 2 diagonal, 3 cleft, 4 square
+        blockImages.add(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.empty), blockWidth, blockWidth, false));
+        blockImages.add(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.wedge), blockWidth, blockWidth, false));
+        blockImages.add(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.diagonal), blockWidth, blockWidth, false));
+        blockImages.add(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.cleft), blockWidth, blockWidth, false));
+        blockImages.add(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.square), blockWidth, blockWidth, false));
+
+        //init blocks at the right coordinates
+        //TODO: remove random image thing
+        for(int x = 0; x < XBlocks; x++) {
+            for(int y = 0; y < YBlocks; y++) {
+                grid[x][y] = new Block(Block.BlockType.EMPTY, blockImages.get((int)(Math.random() * 100) % 5), x * blockWidth, y * blockWidth);
+            }
+        }
 
         getHolder().addCallback(this);
         gameThread = new GameThread(getHolder(), this);
@@ -68,13 +92,18 @@ public class GameWindow extends SurfaceView implements SurfaceHolder.Callback
     public boolean onTouchEvent(MotionEvent event)
     {
         if(event.getAction() == MotionEvent.ACTION_DOWN) {
-            //get the block and rotate it
-            System.out.println("press at " + event.getX() + "," + event.getY());
+
             return true; // VERY IMPORTANT
         }
         if(event.getAction() == MotionEvent.ACTION_UP) {
+            //get the block and rotate it
+            int x = (int)(event.getX() / blockWidth);
+            int y = (int)(event.getY() / blockWidth);
+            System.out.println("press up at " + x + "," + y);
+            grid[x][y].rotate();
+            //grid[x][y].setChanged(true);
+
             //probably don't need anything here
-            System.out.println("stopped pressing");
             return true; // VERY IMPORTANT
         }
 
@@ -91,29 +120,15 @@ public class GameWindow extends SurfaceView implements SurfaceHolder.Callback
     @Override
     public void draw(Canvas canvas)
     {
-        //super.draw(canvas);
-        //need to scale by the number of blocks on the screen
-        final Bitmap bg = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.shapes_background_test),
-                canvas.getWidth(), (int) (canvas.getWidth() * ((float)YBlocks / (float)XBlocks)), false);
-
-        //Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.wedge);
-        //b = Bitmap.createScaledBitmap(b, canvas.getWidth() / XBlocks, canvas.getHeight() / YBlocks, false);
-        //b = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.wedge), bg.getWidth() / 8, bg.getHeight() / 10, false);
-        final Block testBlock = new Block(Block.BlockType.WEDGE,
-                Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.wedge),
-                bg.getWidth() / XBlocks, bg.getHeight() / YBlocks, false), 0, 0);
-
-        //final float scaleFactorX = canvas.getWidth() / (float)bg.getWidth();
-
         if(canvas != null) {
-            final int savedState = canvas.save();
-            //canvas.scale(scaleFactorX, scaleFactorX); //scale Y by same as x? keep square shape?
-
-            canvas.drawBitmap(bg, 0, 0, null);
-            //grid[0][0].draw(canvas);
-            testBlock.draw(canvas);
-
-            //canvas.restoreToCount(savedState); // return to original scaled state (won't keep scaling)
+            for(int x = 0; x < XBlocks; x++) {
+                for(int y = 0; y < YBlocks; y++) {
+                    //if(grid[x][y].isChanged()) {
+                        grid[x][y].draw(canvas);
+                        //grid[x][y].setChanged(false);
+                    //}
+                }
+            }
         }
     }
 }
